597ce5a5c60dd36ff89cffd41eb56e5ee2201093
Amna <amna0103@gmail.com>
2015-07-30 18:23:08 +0500

    First Commit and Scaffold created for the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '0aac0b09645b0686b2af8f3ef17c620ce34e6841e1607e9a150496a7614e0381244c3a400bbb3e7d9becbd091b2792e18b6bc06da8b837b9808c52fc5127460e'
