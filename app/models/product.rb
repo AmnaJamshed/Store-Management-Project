febddbd985baea18968956bf3223aaa0ad5bb075
Amna <amna0103@gmail.com>
2015-08-03 12:20:29 +0500

    Image deletion ability added

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ActiveRecord::Base
 
   has_many :attachments, as: :attachable
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, allow_destroy: :true
 end
